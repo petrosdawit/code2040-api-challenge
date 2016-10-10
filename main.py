277524f95fe5a835746eb66ae872bab6d02e0ce3
petrosdawit <petros_dawit@brown.edu>
2016-10-10 18:26:00 -0400

    step2

--- main.py
+++ main.py
@@ -2,18 +2,37 @@
 # email: petros_dawit@brown.edu
 
 import json, requests
+from StringIO import StringIO
 
 class Code2040(object):
 
 	def __init__(self):
 		url = 'http://challenge.code2040.org/api/register'
-		api_token = 'a4fc745af73d19fc0356b3cc4b9f1243'
+		self._api_token = 'a4fc745af73d19fc0356b3cc4b9f1243'
 		github_url = 'https://github.com/petrosdawit/code2040-api-challenge'
-		payload = json.dumps({'token' : api_token, 'github' : github_url})
+		payload = {'token' : self._api_token, 'github' : github_url}
+		r = requests.post(url, data = payload)
 
-		r = requests.post(url, params = payload)	
-		print r.url
+	def step2(self):
+		reverse_url = 'http://challenge.code2040.org/api/reverse'
+		payload = {'token' : self._api_token}
+		r = requests.post(reverse_url, data = payload)
+		
+		string = r.text
+		reversed_string = string[::-1]
+		
+		validate_url = 'http://challenge.code2040.org/api/reverse/validate' 
+		payload = {'token' : self._api_token, 'string' : reversed_string}
+		r = requests.post(validate_url, data = payload)
+
+	def step3(self):
+		haystack_url = http://challenge.code2040.org/api/haystack
+		payload = {'token' : self._api_token}
+		r = requests.post(haystack_url, data = payload)
+		print r.text
+
 
 if __name__ == '__main__':
 	code2040 = Code2040()
-
+	code2040.step2()
+	code2040.step3()
