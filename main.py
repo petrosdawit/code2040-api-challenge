09530fc39e5ef192b139541ea01bb71de7da436f
petrosdawit <petros_dawit@brown.edu>
2016-10-10 18:32:31 -0400

    step 3

--- main.py
+++ main.py
@@ -18,19 +18,34 @@ class Code2040(object):
 		payload = {'token' : self._api_token}
 		r = requests.post(reverse_url, data = payload)
 		
+		#reversing the string
 		string = r.text
 		reversed_string = string[::-1]
-		
+
 		validate_url = 'http://challenge.code2040.org/api/reverse/validate' 
 		payload = {'token' : self._api_token, 'string' : reversed_string}
 		r = requests.post(validate_url, data = payload)
 
 	def step3(self):
-		haystack_url = http://challenge.code2040.org/api/haystack
+		haystack_url = 'http://challenge.code2040.org/api/haystack'
 		payload = {'token' : self._api_token}
 		r = requests.post(haystack_url, data = payload)
-		print r.text
 
+		#the first index was the needle, the second was the haystack list. I checked for this
+		#by printing out r.text
+		needle = r.text[0]
+		haystack = r.text[1]
+
+		#looping through the haystack and looking for the needle
+		n = len(haystack)
+		index = 0
+		for i in range(len(haystack)):
+			if needle == haystack[i]:
+				index = i
+
+		validate_url = 'http://challenge.code2040.org/api/haystack/validate'
+		payload = {'token' : self._api_token, 'needle' : index}
+		r = requests.post(validate_url, data = payload)
 
 if __name__ == '__main__':
 	code2040 = Code2040()
