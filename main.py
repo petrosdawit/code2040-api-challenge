50c11e3a0f5b01028dab3c6e650a8564f44c6179
petrosdawit <petros_dawit@brown.edu>
2016-10-10 23:13:00 -0400

    step 5. complete

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 # email: petros_dawit@brown.edu
 
 import json, requests
-from StringIO import StringIO
+from datetime import timedelta
+import dateutil.parser
 
 class Code2040(object):
 
+	"""	
+		Here is my Code2040 class. It covers all 5 steps (registration, reverse string, needle in haystack
+		prefix and dating). This coding challenge was finished in one night on Oct 10, 2016
+	"""
+
 	def __init__(self):
+		#headers and api_token will be used for each method when making the http post request
 		self._headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 		self._api_token = 'a4fc745af73d19fc0356b3cc4b9f1243'
 
+		#registering
 		url = 'http://challenge.code2040.org/api/register'
 		github_url = 'https://github.com/petrosdawit/code2040-api-challenge'
 		payload = {'token' : self._api_token, 'github' : github_url}
@@ -61,24 +69,44 @@ class Code2040(object):
 		prefix = data['prefix']
 		array = data['array']
 
+		#going through array and checking each string in array if it DOES NOT start with the prefix
+		#if it doesn't, I append it to the result_array
 		result_array = []
 		n = len(array)
 		for i in range(n):
 			if not array[i].startswith(prefix):
 				result_array.append(array[i])
 
-		headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
-
 		validate_url = 'http://challenge.code2040.org/api/prefix/validate'
 		payload = {'token' : self._api_token, 'array' : result_array}
 		r = requests.post(validate_url, data = json.dumps(payload), headers = self._headers)
 		print r.text
 
+	def step5(self):
+		prefix_url = 'http://challenge.code2040.org/api/dating'
+		payload = {'token' : self._api_token}
+		r = requests.post(prefix_url, data = json.dumps(payload), headers = self._headers)
+
+		data = json.loads(r.text)
+		datestamp = data['datestamp']
+		interval = data['interval']
+
+		#parse through the datestamp to get a time and add to it the interval converted into seconds
+		#I then get rid of the timezone and put it back to iso8061 format
+		dt = dateutil.parser.parse(datestamp) + timedelta(seconds=interval)		 
+		dt = dt.replace(tzinfo=None)
+		dt = dt.isoformat() + 'Z'
+
+		validate_url = 'http://challenge.code2040.org/api/dating/validate'
+		payload = {'token' : self._api_token, 'datestamp' : dt}
+		r = requests.post(validate_url, data = json.dumps(payload), headers = self._headers)
+		print r.text
 
 if __name__ == '__main__':
 	code2040 = Code2040()
 	code2040.step2()
 	code2040.step3()
 	code2040.step4()
+	code2040.step5()
 
 
