53f2e531ecea9961fc3be6950f611435d0f8c3f4
petrosdawit <petros_dawit@brown.edu>
2016-10-10 22:50:27 -0400

    step 4

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ from StringIO import StringIO
 class Code2040(object):
 
 	def __init__(self):
-		url = 'http://challenge.code2040.org/api/register'
+		self._headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 		self._api_token = 'a4fc745af73d19fc0356b3cc4b9f1243'
+
+		url = 'http://challenge.code2040.org/api/register'
 		github_url = 'https://github.com/petrosdawit/code2040-api-challenge'
 		payload = {'token' : self._api_token, 'github' : github_url}
-		r = requests.post(url, data = payload)
+		r = requests.post(url, data = json.dumps(payload), headers = self._headers)
 		print r.text
 
 	def step2(self):
 		reverse_url = 'http://challenge.code2040.org/api/reverse'
 		payload = {'token' : self._api_token}
-		r = requests.post(reverse_url, data = payload)
+		r = requests.post(reverse_url, data = json.dumps(payload), headers = self._headers)
 		
 		#reversing the string
 		string = r.text
@@ -25,13 +27,13 @@ class Code2040(object):
 
 		validate_url = 'http://challenge.code2040.org/api/reverse/validate' 
 		payload = {'token' : self._api_token, 'string' : reversed_string}
-		r = requests.post(validate_url, data = payload)
+		r = requests.post(validate_url, data = json.dumps(payload), headers = self._headers)
 		print r.text
 
 	def step3(self):
 		haystack_url = 'http://challenge.code2040.org/api/haystack'
 		payload = {'token' : self._api_token}
-		r = requests.post(haystack_url, data = payload)
+		r = requests.post(haystack_url, data = json.dumps(payload), headers = self._headers)
 
 		#loading json
 		data = json.loads(r.text)
@@ -41,41 +43,42 @@ class Code2040(object):
 		#looping through the haystack and looking for the needle
 		n = len(haystack)
 		index = 0
-		for i in range(len(haystack)):
+		for i in range(n):
 			if needle == haystack[i]:
 				index = i
 
 		validate_url = 'http://challenge.code2040.org/api/haystack/validate'
 		payload = {'token' : self._api_token, 'needle' : index}
-		r = requests.post(validate_url, data = payload)
+		r = requests.post(validate_url, data = json.dumps(payload), headers = self._headers)
 		print r.text
 
 	def step4(self):
 		prefix_url = 'http://challenge.code2040.org/api/prefix'
 		payload = {'token' : self._api_token}
-		r = requests.post(prefix_url, data = payload)
+		r = requests.post(prefix_url, data = json.dumps(payload), headers = self._headers)
 
-		#loading json
 		data = json.loads(r.text)
 		prefix = data['prefix']
 		array = data['array']
 
-		#create a new return_list. check each word in the array if it DOES NOT start with the prefix. if it
-		#doesn't add it to our return list
-		return_list = []
-		for word in array:
-			if not word.startswith(prefix):
-				return_list.append(word)
+		result_array = []
+		n = len(array)
+		for i in range(n):
+			if not array[i].startswith(prefix):
+				result_array.append(array[i])
+
+		headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 
 		validate_url = 'http://challenge.code2040.org/api/prefix/validate'
-		payload = {'token' : self._api_token, 'array' : return_list}
-		r = requests.post(validate_url, data = payload)
+		payload = {'token' : self._api_token, 'array' : result_array}
+		r = requests.post(validate_url, data = json.dumps(payload), headers = self._headers)
 		print r.text
-		
+
+
 if __name__ == '__main__':
 	code2040 = Code2040()
 	code2040.step2()
 	code2040.step3()
-	# code2040.step4()
+	code2040.step4()
 
 
